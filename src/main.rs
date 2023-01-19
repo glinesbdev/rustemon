29e011cb2ea7a3e9ff998fc10853f830ff27a644
Bradyn Glines <glinesbdev@gmail.com>
2023-01-19 16:22:27 -0500

    Remove main.rs file

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use rustemon::*;
-
-#[tokio::main]
-async fn main() -> std::result::Result<(), ResponseError> {
-    let data: SetsResponse = Request::new("sets").query("series:base").search().await?;
-    println!("{data:#?}");
-
-    Ok(())
-}
